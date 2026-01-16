8242119d3c2e727d35f222b5580f66cb3adfb791
Tamaghna <tamaghna@morismedia.net>
2026-01-17 01:57:30 +0530

    Refactoring for Landing page performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,10 +74,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
