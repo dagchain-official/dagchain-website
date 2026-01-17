4d836bf5f1bceacbbb12c0548780b26585467cfd
Tamaghna <tamaghna@morismedia.net>
2026-01-17 18:21:36 +0530

    Refactoring for Landing page performance

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,23 @@ import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
 import { useVisitorTracking } from "@/lib/useVisitorTracking";
 
+const SectionSkeleton = () => <div className="h-[400px] w-full bg-slate-900/20 animate-pulse" />;
+
 // Below-the-fold components are loaded dynamically
-const About = dynamic(() => import('@/components/about').then(mod => ({ ssr: false, loading: () => null, default: mod.About })));
-const Technology = dynamic(() => import('@/components/technology').then(mod => ({ ssr: false, loading: () => null, default: mod.Technology })));
-const LogoLoopSection = dynamic(() => import("@/components/logo-loop").then(mod => ({ ssr: false, loading: () => null, default: mod.LogoLoopSection })));
-const Roadmap = dynamic(() => import('@/components/roadmap').then(mod => ({ ssr: false, loading: () => null, default: mod.Roadmap })));
-const Tokenomics = dynamic(() => import('@/components/tokenomics').then(mod => ({ ssr: false, loading: () => null, default: mod.Tokenomics })));
-const Community = dynamic(() => import('@/components/community').then(mod => ({ ssr: false, loading: () => null, default: mod.Community })));
-const Footer = dynamic(() => import('@/components/footer').then(mod => ({ ssr: false, loading: () => null, default: mod.Footer })));
-const Ecosystem = dynamic(() => import("@/components/ecosystem").then(mod => ({ ssr: false, default: mod.Ecosystem })));
-const DagArmy = dynamic(() => import("@/components/dag-army").then(mod => ({ ssr: false, loading: () => null, default: mod.DagArmy })));
-const RevolutionCTA = dynamic(() => import("@/components/revolution-cta").then(mod => ({ ssr: false, loading: () => null, default: mod.RevolutionCTA })));
-const BookMeeting = dynamic(() => import("@/components/book-meeting").then(mod => ({ ssr: false, loading: () => null, default: mod.BookMeeting })));
+const About = dynamic(() => import('@/components/about').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.About })));
+const Technology = dynamic(() => import('@/components/technology').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Technology })));
+const LogoLoopSection = dynamic(() => import("@/components/logo-loop").then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.LogoLoopSection })));
+const Roadmap = dynamic(() => import('@/components/roadmap').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Roadmap })));
+const Tokenomics = dynamic(() => import('@/components/tokenomics').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Tokenomics })));
+const Community = dynamic(() => import('@/components/community').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Community })));
+const Footer = dynamic(() => import('@/components/footer').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Footer })));
+const Ecosystem = dynamic(() => import("@/components/ecosystem").then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.Ecosystem })));
+const DagArmy = dynamic(() => import("@/components/dag-army").then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.DagArmy })));
+const RevolutionCTA = dynamic(() => import("@/components/revolution-cta").then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.RevolutionCTA })));
+const BookMeeting = dynamic(() => import("@/components/book-meeting").then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.BookMeeting })));
 
 // Modals should always be dynamic (0kb on initial load)
-const WaitlistModal = dynamic(() => import('@/components/waitlist-modal').then(mod => ({ ssr: false, loading: () => null, default: mod.WaitlistModal })));
+const WaitlistModal = dynamic(() => import('@/components/waitlist-modal').then(mod => ({ ssr: true, loading: () => <SectionSkeleton />, default: mod.WaitlistModal })));
 
 export default function Home() {
   const [showWaitlist, setShowWaitlist] = useState(false);
